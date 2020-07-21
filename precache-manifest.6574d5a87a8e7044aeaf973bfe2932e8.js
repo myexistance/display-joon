231860bbcc8ebe31dde0ed8d80f99dc715aea618
myexistance <joonyoonlives@gmail.com>
2020-07-21 20:20:14 +1200

    Updates

---
+++ precache-manifest.6574d5a87a8e7044aeaf973bfe2932e8.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab9f6bf9000567bb9d8932744f582514",
+    "url": "/display-joon/index.html"
+  },
+  {
+    "revision": "81d70b01c7ced07e1dbf",
+    "url": "/display-joon/static/css/2.812d3d72.chunk.css"
+  },
+  {
+    "revision": "d17cde2ecccc7528cb3a",
+    "url": "/display-joon/static/css/main.16dc108f.chunk.css"
+  },
+  {
+    "revision": "81d70b01c7ced07e1dbf",
+    "url": "/display-joon/static/js/2.5e284307.chunk.js"
+  },
+  {
+    "revision": "0f24eaf990f24b4c8eca5dc0d1b78678",
+    "url": "/display-joon/static/js/2.5e284307.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d17cde2ecccc7528cb3a",
+    "url": "/display-joon/static/js/main.f621b6b8.chunk.js"
+  },
+  {
+    "revision": "e028ec40c94133cd29b5",
+    "url": "/display-joon/static/js/runtime-main.682c3b41.js"
+  },
+  {
+    "revision": "f534691ce0df9f23aace0a4075f597fa",
+    "url": "/display-joon/static/media/Argenda.f534691c.jpg"
+  },
+  {
+    "revision": "6cbec58235bcc812a3190ff51d13fa8d",
+    "url": "/display-joon/static/media/Logo.6cbec582.png"
+  },
+  {
+    "revision": "e6218907c8169c64bfee50d7928cbb8b",
+    "url": "/display-joon/static/media/Logo1.e6218907.png"
+  },
+  {
+    "revision": "65445111aebd93ece87886c83b891062",
+    "url": "/display-joon/static/media/coffeeFinder.65445111.jpg"
+  },
+  {
+    "revision": "fb8afbfc0f689de58dfbc6d10931fa84",
+    "url": "/display-joon/static/media/coffeeFinderUX.fb8afbfc.jpg"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/display-joon/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/display-joon/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/display-joon/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/display-joon/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/display-joon/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/display-joon/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/display-joon/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/display-joon/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/display-joon/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/display-joon/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "b5176f9613974474226413eea4c1d125",
+    "url": "/display-joon/static/media/news.b5176f96.jpg"
+  },
+  {
+    "revision": "4c68965961210871e3a0e4c4eb3d348c",
+    "url": "/display-joon/static/media/newsfinder.4c689659.jpg"
+  },
+  {
+    "revision": "080f1f9c725dcfe6a824849942ed5a36",
+    "url": "/display-joon/static/media/stripeapi.080f1f9c.png"
+  }
+]);
